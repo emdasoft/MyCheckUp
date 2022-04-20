64af1f9dd556cf238248aaa90d9ae1f9f4f74f23
V.Kukh <emdasoftware@gmail.com>
2022-04-20 12:16:12 +0300

    add_checkBoxes

--- app/src/main/java/com/emdasoft/mycheckup/MainActivity.kt
+++ app/src/main/java/com/emdasoft/mycheckup/MainActivity.kt
@@ -14,20 +14,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         openFragment(MainFragment.newInstance())
-        binding.bottomNav.setOnNavigationItemSelectedListener {
-            when(it.itemId){
-                R.id.home -> openFragment(MainFragment.newInstance())
-                R.id.cards -> openFragment(ResultFragment.newInstance())
-            }
-            true
-        }
     }
 
     private fun openFragment(f: Fragment) {
         supportFragmentManager
             .beginTransaction()
-            .setCustomAnimations(R.anim.from_right_to_left, R.anim.exit_rigth_to_left,
-            R.anim.from_left_to_right, R.anim.exit_left_to_right)
             .replace(R.id.placeHolder, f)
             .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
             .commit()
