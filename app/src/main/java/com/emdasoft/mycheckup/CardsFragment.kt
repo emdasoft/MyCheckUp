db5913fbea1d6de230e792801cc6ea7bd24ec1f4
GesserWy <emdasoftware@gmail.com>
2022-06-15 22:16:29 +0300

    rebuild_card_fr

--- app/src/main/java/com/emdasoft/mycheckup/CardsFragment.kt
+++ app/src/main/java/com/emdasoft/mycheckup/CardsFragment.kt
@@ -33,19 +33,38 @@ class CardsFragment : Fragment(), CardsAdapter.Listener {
 
             viewPager2 = viewPager
             val cards: MutableList<Card> = ArrayList()
-            cards.add(Card("Cashalot", 258.40, "BYN", "POV"))
-            cards.add(Card("БелВЭБ", 113.52, "BYN", "POV"))
-            cards.add(Card("Резерв", 550.0, "BYN", "POV"))
-            cards.add(Card("МТ", 137.04, "BYN", "POV"))
-            cards.add(Card("Наличные USD", 2750.0, "USD", "1"))
-            cards.add(Card("Мелочь USD", 300.0, "USD", "2"))
-            cards.add(Card("Наличные EUR", 980.0, "EUR", "3"))
-            cards.add(Card("FinStore Инвестиции", 1020.0, "USD", "3"))
-            cards.add(Card("FinStore Доход", 3.52, "USD", "4"))
-            cards.add(Card("Отложенные BYN", 100.04, "BYN", "6"))
+            cards.add(Card("Cashalot", 52.23, "BYN", "POV"))
+            cards.add(Card("БелВЭБ", 156.89, "BYN", "POV"))
+            cards.add(Card("Наличные BYN", 96.0, "BYN", "POV"))
+            cards.add(Card("Резерв", 400.0, "BYN", "RES"))
+            cards.add(Card("МТ", 176.69, "BYN", "MT"))
+            cards.add(Card("Наличные USD", 2750.0, "USD", "SEB"))
+            cards.add(Card("Мелочь USD", 300.0, "USD", "SEB"))
+            cards.add(Card("Наличные EUR", 980.0, "EUR", "SEB"))
+            cards.add(Card("FinStore Инвестиции", 1020.0, "USD", "SEB"))
+            cards.add(Card("FinStore Доход", 3.52, "USD", "SEB"))
+            cards.add(Card("Отложенные BYN", 300.0, "BYN", "SEB"))
 
             var total = 0.0
+            var pov = 0.0
+            var res = 0.0
+            var mt = 0.0
+            var seb = 0.0
+
+
             for (item in cards) {
+                if (item.description == "POV"){
+                    pov += item.amount
+                }
+                if (item.description == "RES"){
+                    res += item.amount
+                }
+                if (item.description == "MT"){
+                    mt += item.amount
+                }
+                if (item.description == "SEB"){
+                    seb += item.amount
+                }
                 if (item.currency == "USD") {
                     total += item.amount
                 }
@@ -57,7 +76,10 @@ class CardsFragment : Fragment(), CardsAdapter.Listener {
                 }
             }
             total = (total * 100).roundToInt() / 100.00
-
+            tvTotalPov.text = "Total POV is : $pov"
+            tvTotalRes.text = "Total RES is : $res"
+            tvTotalMt.text = "Total MT is : $mt"
+            tvTotalSeb.text = "Total SEB is : $seb"
             val tmpText = "Total amount $total USD"
             tvTotal.text = tmpText
 
